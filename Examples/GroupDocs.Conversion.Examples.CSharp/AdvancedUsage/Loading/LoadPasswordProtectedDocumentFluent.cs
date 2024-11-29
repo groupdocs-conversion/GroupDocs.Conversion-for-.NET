23f45b16e4546fb9a184357eebf100fd00897867
Nikola Yankov <nikola.yankov@aspose.com>
2024-11-29 20:04:53 +0200

    GroupDocs.Conversion for .NET 24.11 samples update

--- Examples/GroupDocs.Conversion.Examples.CSharp/AdvancedUsage/Loading/LoadPasswordProtectedDocumentFluent.cs
+++ Examples/GroupDocs.Conversion.Examples.CSharp/AdvancedUsage/Loading/LoadPasswordProtectedDocumentFluent.cs
@@ -16,7 +16,7 @@ namespace GroupDocs.Conversion.Examples.CSharp.AdvancedUsage
             string outputFolder = Constants.GetOutputDirectoryPath();
             string outputFile = Path.Combine(outputFolder, "converted.pdf");
 
-            FluentConverter.Load(Constants.SAMPLE_DOCX_WITH_PASSWORD).WithOptions(() => new WordProcessingLoadOptions
+            FluentConverter.Load(Constants.SAMPLE_DOCX_WITH_PASSWORD).WithOptions((LoadContext loadContext) => new WordProcessingLoadOptions
                 {
                     Password = "12345"
                 })
